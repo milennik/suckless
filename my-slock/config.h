5f68dbc92e874f701d824bb6a72cce3d94c21fb7
nikola <nikola.milenkovic@outlook.com>
2021-11-28 01:07:12 +0100

    my-dwm

--- my-slock/config.h
+++ my-slock/config.h
@@ -4,8 +4,8 @@ static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#5E81AC",   /* during input */
-	[FAILED] = "#B48EAD",   /* wrong password */
+	[INPUT] =  "#a9b665",   /* during input */
+	[FAILED] = "#d3869b",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
