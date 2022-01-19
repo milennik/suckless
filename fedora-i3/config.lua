6bd29305272c669fa1d0931da93beecc57a625ff
nikola <nikola.milenkovic@outlook.com>
2022-01-20 00:55:44 +0100

    fzf lvim golan rofi palenight

--- fedora-i3/config.lua
+++ fedora-i3/config.lua
@@ -81,15 +81,19 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- generic LSP settings
 
 -- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
+lvim.lsp.automatic_servers_installation = false
 
 -- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
 -- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
 -- vim.list_extend(lvim.lsp.override, { "pyright" })
 
 -- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
+local opts = {} -- check the lspconfig documentation for a list of all possible options
+require("lvim.lsp.manager").setup("pylsp", opts)
+
+-- ocal opts = {} -- check the lspconfig documentation for a list of all possible options
+-- equire("lvim.lsp.manager").setup("gopls", opts)
+
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
@@ -143,6 +147,7 @@ lvim.plugins = {
     {"shaunsingh/nord.nvim"},
     {"rebelot/kanagawa.nvim"},
     {"drewtempelmeyer/palenight.vim"},
+    {"ray-x/go.nvim"},
     {
       "folke/trouble.nvim",
       cmd = "troubletoggle",
